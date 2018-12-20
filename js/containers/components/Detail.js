a6e49a3f22fc1a5e4adc6d251de00d7c3fef8dd5
Code4GL <guanli1991@163.com>
2018-12-20 15:05:48 +0800

    detail样式初定

--- js/containers/components/Detail.js
+++ js/containers/components/Detail.js
@@ -2,7 +2,7 @@
  * @Author: Code4GL
  * @Date: 2018-12-19 17:46:15
  * @Last Modified by: Code4GL
- * @Last Modified time: 2018-12-19 18:11:19
+ * @Last Modified time: 2018-12-20 15:05:25
  */
 
 'use strict';
@@ -18,18 +18,73 @@ export default class Detail extends Component {
     const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <Text>{navigation.state.params.detail.name}</Text>
-        <Text>Detail</Text>
-        <Text>english</Text>
-        <Text>中文</Text>
-        <TouchableOpacity><Text>{navigation.state.params.detail.name}</Text></TouchableOpacity>
-        <Text>propsList</Text>
-        <Text>methodsList</Text>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>{navigation.state.params.detail.name}</Text>
+        </View>
+        {/* 中英文描述 */}
+        <View>
+          <View>
+            <Text>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {navigation.state.params.detail.description}
+            </Text>
+          </View>
+          <View style={styles.translation}>
+            <View style={styles.translationTitle}>
+              <Text style={styles.translationText}>翻译：</Text>
+            </View>
+            <Text>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {navigation.state.params.detail.descriptionCN}
+            </Text>
+          </View>
+        </View>
+        {/* 组件效果展示 */}
+        <View>
+          <View style={styles.exampleTitle}>
+            <Text style={styles.exampleTitleText}>Example</Text>
+          </View>
+          <TouchableOpacity>
+            <Text>{navigation.state.params.detail.name}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1}
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF'
+  },
+  title: {
+    justifyContent: 'center'
+  },
+  titleText: {
+    fontSize: 35,
+    padding: 10,
+    fontWeight: 'bold',
+    color: 'rgba(0,81,102,1)'
+  },
+  translation: {
+    backgroundColor: 'lightgray'
+  },
+  translationTitle: {
+    justifyContent: 'center'
+  },
+  translationText: {
+    color: 'green',
+    padding: 5,
+    fontWeight: 'bold'
+  },
+  exampleTitle: {
+    justifyContent: 'center'
+  },
+  exampleTitleText: {
+    fontSize: 20,
+    padding: 5,
+    fontWeight: 'bold',
+    color: 'rgba(0,81,102,1)'
+  }
 });
