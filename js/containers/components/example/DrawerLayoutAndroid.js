ef01494fc559dc3a2bd973e7cfdf5d9c8c2401f8
Code4GL <guanli1991@163.com>
2018-12-22 12:32:49 +0800

    DrawerLayoutAndroid添加

--- js/containers/components/example/DrawerLayoutAndroid.js
+++ js/containers/components/example/DrawerLayoutAndroid.js
@@ -8,15 +8,27 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text,DrawerLayoutAndroid} from 'react-native';
 
-export default class DrawerLayoutAndroid extends Component {
+export default class DrawerLayoutAndroidScreen extends Component {
 
   render () {
-    return (
-      <View style={styles.container}>
-        <Text>DrawerLayoutAndroid</Text>
+    var navigationView = (
+      <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
       </View>
+    );
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Plesae swipe right！</Text>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>请向右滑动！</Text>
+        </View>
+      </DrawerLayoutAndroid>
     );
   }
 }
