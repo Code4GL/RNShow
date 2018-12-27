ce883457d87b9a68ae9c0609d8b1ed155d9c37b6
Code4GL <guanli1991@163.com>
2018-12-27 10:02:59 +0800

    第三方模块添加

--- js/containers/thirdParty/index.js
+++ js/containers/thirdParty/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Code4GL
- * @Date: 2018-12-17 15:24:46
+ * @Date: 2018-12-17 15:24:06
  * @Last Modified by: Code4GL
- * @Last Modified time: 2018-12-17 15:32:15
+ * @Last Modified time: 2018-12-27 10:00:17
  */
 
 'use strict';
@@ -14,7 +14,7 @@ export default class Index extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text>Basics</Text>
+        <Text>Third party</Text>
       </View>
     );
   }
--- js/navigation/BottomTabNavigator.js
+++ js/navigation/BottomTabNavigator.js
@@ -2,7 +2,7 @@
  * @Author: Code4GL
  * @Date: 2018-12-17 14:59:33
  * @Last Modified by: Code4GL
- * @Last Modified time: 2018-12-18 17:31:06
+ * @Last Modified time: 2018-12-27 10:01:48
  */
 
 'use strict';
@@ -14,22 +14,18 @@ import {
 
 import About from '../containers/about/index';
 import APIs from '../containers/apis/index';
-import Basics from '../containers/basics/index';
 import Components from '../containers/components/index';
-import Guides from '../containers/guides/index';
+import ThirdParty from '../containers/thirdParty/index';
 
 const BottomTabNavigator = createBottomTabNavigator({
-  Basics: {
-    screen: Basics
-  },
-  Guides: {
-    screen: Guides
-  },
   Components: {
     screen: Components
   },
   APIs: {
     screen: APIs
+  },
+  ThirdParty: {
+    screen: ThirdParty
   },
   About: {
     screen: About
